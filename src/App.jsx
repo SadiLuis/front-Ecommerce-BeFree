1df56df24fc24dccf39d0c1cfd42957102e9888d
SadiLuis <sadiluisrueda.94@gmail.com>
2023-08-03 17:21:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  
+
+  return (
+    <>
+     <header className="text-red-600">
+      <nav>
+        <ul >
+          <li><a href="/">Home</a></li>
+          <li>  About Us</li>
+        </ul>
+      </nav>
+     </header>
+    </>
+  )
+}
+
+export default App
